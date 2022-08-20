e93ef7a9b1301f7760f4e35125ac9fc883ce3ae2
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-08-20 18:41:48 +0300

    commit tree b6e0cb1129ef10ed4ec7cfd921e192c65f937c1a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include? "."
   end
 
   def question?
-
+    self.include? "?"
   end
 
   def exclamation?
-
+    self.include? "!"
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
